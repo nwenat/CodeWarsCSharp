3f2119c4da898ac646f76c7c18e1d66b8e8c4600
nwenat <natala.dorozynska@gmail.com>
2021-06-12 10:41:27 +0200

    StringEndsWith

---
+++ StringEndsWith/StringEndsWith/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringEndsWith
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution("samurai", "ai"));
+        }
+
+        public static bool Solution(string str, string ending)
+        {
+            if (str.EndsWith(ending))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
