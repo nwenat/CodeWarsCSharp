3d68bc0c4a68240c54071db39ed1125e178ac900
nwenat <natala.dorozynska@gmail.com>
2021-06-14 20:37:27 +0200

    ReversingAProcess scratch

---
+++ ReversingAProcess/ReversingAProcess/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ReversingAProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Decode("1234567890gf"));
+            //Console.WriteLine(Decode("1273409kuqhkoynvvknsdwljantzkpnmfgf"));
+            Console.WriteLine(Decode("5057aan"));
+            Console.WriteLine("uogbucwnddunktsjfanzlurnyxmx");
+        }
+
+        public static String Decode(String r)
+        {
+            int x = Regex.Match(r, "[a-z]").Index;
+            int j = Int32.Parse(r.Substring(0, x));
+            string code = r.Substring(x, r.Length - x);
+
+            string result = "";
+
+            foreach (char c in code)
+            {
+                int a = (int)c - 97;
+
+                char? b = null;
+                for (int i = 0; i < 26; i++)
+                {
+                    if (i * j % 26 == a)
+                    {
+                        b = (char)(i + 97);
+                        result += b;
+                        break;
+                    }
+                    if (i == 25 && b.Equals(null))
+                    {
+                        return "Impossible to decode";
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
