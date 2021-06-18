bceb3d7adfe470f26dac8b73f3af7a8a33f98ff6
nwenat <natala.dorozynska@gmail.com>
2021-06-18 11:13:57 +0200

    Heron'sFormula

---
+++ Heron'sFormula/Heron'sFormula/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Heron_sFormula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(heron(3, 4, 5));
+        }
+
+        public static double heron(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+    }
+}
