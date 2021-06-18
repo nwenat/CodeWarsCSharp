da5219ecf59ab17c5dc02d566e1606a985ec8424
nwenat <natala.dorozynska@gmail.com>
2021-06-18 11:04:04 +0200

    OverTheRoad answer

--- OverTheRoad/OverTheRoad/Program.cs
+++ OverTheRoad/OverTheRoad/Program.cs
@@ -11,22 +11,7 @@ namespace OverTheRoad
 
         public static long overTheRoad(long address, long n)
         {
-            int[] arr = new int[n];
-            int k;
-            if (address % 2 == 0)
-            {
-                k = 1;
-            }
-            else
-            {
-                k = 2;
-            }
-            for (int i = 0; i < n; i++)
-            {
-                arr[i] = k;
-                k += 2;
-            }
-            return arr[(n - 1) - (address /2)];
+            return 2 * n - address + 1;
         }
     }
 }
