88aa79b0fc3b91b628b42262d26fe01b512db4ce
nwenat <natala.dorozynska@gmail.com>
2021-06-21 12:57:04 +0200

    Two-DimensionalArrayOperation

---
+++ Two-DimensionalArrayOperation/Two-DimensionalArrayOperation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Two_DimensionalArrayOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Result(new int[,] { { 7, 1, 5, 8 }, { 2, 1, 4, 9 }, { 3, 2, 4, 7 } }));
+        }
+
+        public static int Result(int[,] array)
+        {
+            int n = array.GetLength(0);
+            int sum = 0;
+            int result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < array.Length / n ; j++)
+                {
+                    sum += array[i, j];
+                }
+                Console.WriteLine(sum);
+                result *= sum;
+                sum = 0;
+            }
+            return result;
+        }
+    }
+}
