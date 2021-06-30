6768a0c4c4a0625064720c7d2079697f93fc5dcf
nwenat <natala.dorozynska@gmail.com>
2021-06-30 11:13:13 +0200

    CountByX

---
+++ CountByX/CountByX/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CountByX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountBy(2, 5));
+        }
+
+        public static int[] CountBy(int x, int n)
+        {
+            int[] z = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                z[i] = x * (i + 1);
+                Console.WriteLine(z[i]);
+            }
+
+            return z;
+        }
+    }
+}
