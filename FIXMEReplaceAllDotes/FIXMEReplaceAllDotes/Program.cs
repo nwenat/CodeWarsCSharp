a275883b9976b7db9742b4197252336192e0bb64
nwenat <natala.dorozynska@gmail.com>
2021-07-03 13:26:17 +0200

    FIXMEReplaceAllDotes

---
+++ FIXMEReplaceAllDotes/FIXMEReplaceAllDotes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FIXMEReplaceAllDotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReplaceDots("one.two.three"));
+        }
+
+        public static string ReplaceDots(string str)
+        {
+            return new Regex("\\.").Replace(str, "-");
+        }
+    }
+}
