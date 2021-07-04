af260473d9af4679e22ec477d5f8213f30c7a44d
nwenat <natala.dorozynska@gmail.com>
2021-07-04 09:35:17 +0200

    CompletenaryDNA

---
+++ ComplementaryDNA/ComplementaryDNA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ComplementaryDNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MakeComplement("TAACG"));
+        }
+
+        public static string MakeComplement(string dna)
+        {
+            string rezult = "";
+            for (int i = 0; i < dna.Length; i++)
+            {
+                switch (dna[i])
+                {
+                    case 'A':
+                        rezult = rezult + "T";
+                        break;
+                    case 'T':
+                        rezult = rezult + "A";
+                        break;
+                    case 'G':
+                        rezult = rezult + "C";
+                        break;
+                    case 'C':
+                        rezult = rezult + "G";
+                        break;
+                }
+            }
+            return rezult;
+        }
+    }
+}
