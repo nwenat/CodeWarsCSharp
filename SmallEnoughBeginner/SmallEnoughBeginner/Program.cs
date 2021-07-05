858298b820d8cb6a5d8b963837c244aedc1e22e6
nwenat <natala.dorozynska@gmail.com>
2021-07-05 16:44:44 +0200

    SmallEnough?Beginner

---
+++ SmallEnoughBeginner/SmallEnoughBeginner/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallEnoughBeginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SmallEnough(new int[] { 66, 101 }, 200));
+        }
+
+        public static bool SmallEnough(int[] a, int limit)
+        {
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    if (a[i] > limit)
+            //    {
+            //        return false;
+            //    }
+            //}
+            //return true;
+
+            return a.Max() > limit ? false : true;
+        }
+    }
+}
