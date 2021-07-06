47bc0a518c3a7a134792690567bc61388317cb4f
nwenat <natala.dorozynska@gmail.com>
2021-07-06 16:06:10 +0200

    NoOddiesHere

---
+++ NoOdditiesHere/NoOdditiesHere/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoOdditiesHere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NoOdds(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        public static int[] NoOdds(int[] values)
+        {
+            int x = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if(values[i] % 2 == 0)
+                {
+                    x++;
+                }
+            }
+
+            int[] answer = new int[x];
+            x = 0;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] % 2 == 0)
+                {
+                    answer[x] = values[i];
+                    x++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
