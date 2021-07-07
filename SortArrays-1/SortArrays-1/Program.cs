b1bafa24506726018bf135284a7e83a375f767d1
natala.dorozynska@gmail.com <natala.dorozynska@gmail.com>
2021-07-07 12:36:50 +0200

    SortArrays-1

---
+++ SortArrays-1/SortArrays-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortArrays_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SortMe(new[] { "one", "two", "three" }));
+        }
+
+        public static string[] SortMe(string[] names)
+        {
+            Array.Sort(names);
+            return names;
+        }
+    }
+}
