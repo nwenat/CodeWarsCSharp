57ea30df920be31d48c6175efa589f15f5d32b51
nwenat <natala.dorozynska@gmail.com>
2021-07-08 14:15:48 +0200

    NumberOfRectanglesInAGrid

---
+++ NumberOfRectanglesInAGrid/NumberOfRectanglesInAGrid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOfRectanglesInAGrid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfRectangles(4, 4));
+        }
+        public static int NumberOfRectangles(int m, int n)
+        {
+            int answer = 0;
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    answer += (m - i + 1) * (n - j + 1);
+                }
+            }
+            return answer;
+        }
+    }
+}
