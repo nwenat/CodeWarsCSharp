5aa28fbf81b84de285ff6cba1d2173bdd87e8c78
nwenat <natala.dorozynska@gmail.com>
2021-07-20 09:52:40 +0200

    CountTheDivisorsOfANumber

---
+++ CountTheDivisorsOfANumber/CountTheDivisorsOfANumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountTheDivisorsOfANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divisors(54));
+        }
+
+        public static int Divisors(int n)
+        {
+            int answer = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
