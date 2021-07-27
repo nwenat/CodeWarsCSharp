6dbbedf0eb19ec200978dee72f28da4b0136801f
nwenat <natala.dorozynska@gmail.com>
2021-07-27 08:39:22 +0200

    SumOfIntegersInString

---
+++ SumOfInegersInString/SumOfInegersInString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SumOfInegersInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumOfIntegersInString("C4t5 are 4m4z1ng."));
+        }
+
+        public static int SumOfIntegersInString(string s)
+        {
+            string[] numbers = Regex.Split(s, @"\D+");
+            return numbers.Where(x => x != "").Sum(a => Int32.Parse(a));
+
+            //int sum = 0;
+            //int sum2 = 0;
+
+            //foreach (var item in numbers)
+            //{
+            //    if (Int32.TryParse(item, out sum2))
+            //    {
+            //        sum += Int32.Parse(item);
+            //    }
+            //}
+            //return sum;
+        }
+    }
+}
