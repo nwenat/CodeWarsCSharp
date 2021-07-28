7712530b7ec624fb15e18acf7656d854d8840028
nwenat <natala.dorozynska@gmail.com>
2021-07-28 08:29:28 +0200

    SprayingTrees

---
+++ SprayingTrees/SprayingTrees/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprayingTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Task("Wednesday", 10, 2));
+        }
+
+        public static string Task(string w, int n, int c)
+        {
+            string name = "";
+            switch (w)
+            {
+                case "Monday":
+                    name = "James";
+                    break;
+                case "Tuesday":
+                    name = "John";
+                    break;
+                case "Wednesday":
+                    name = "Robert";
+                    break;
+                case "Thursday":
+                    name = "Michael";
+                    break;
+                case "Friday":
+                    name = "William";
+                    break;
+            }
+            return "It is " + w + " today, " + name + ", you have to work, you must spray " + n + " trees and you need " + n * c + " dollars to buy liquid";
+        }
+    }
+}
