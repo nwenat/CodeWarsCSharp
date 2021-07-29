5b503d40c65fd5745b3ec42ff1a619ceeeb8ec77
nwenat <natala.dorozynska@gmail.com>
2021-07-29 08:28:49 +0200

    ValidPhoneNumber

---
+++ ValidPhoneNumber/ValidPhoneNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "(123) 456-7890";
+            Console.WriteLine(ValidPhoneNumber(test));
+        }
+
+        public static bool ValidPhoneNumber(string phoneNumber)
+        {
+            if (phoneNumber.Length != 14 || phoneNumber[0] != '(' || phoneNumber[4] != ')' || phoneNumber[5] != ' ' || phoneNumber[9] != '-')
+            {
+                return false;
+            }
+            return Regex.Replace(phoneNumber, @"[\d]", string.Empty).ToString().Length == 4 ? true: false;
+        }
+    }
+}
