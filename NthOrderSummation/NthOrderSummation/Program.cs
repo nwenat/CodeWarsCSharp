6ad65152b9075172b4198c440ef1bc2c8a0248ac
nwenat <natala.dorozynska@gmail.com>
2021-07-30 08:59:49 +0200

    WhichAreIn

---
+++ NthOrderSummation/NthOrderSummation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NthOrderSummation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(S(2, 5));
+            Console.WriteLine(S(1, 1));
+            Console.WriteLine(S(3, 7));
+            Console.WriteLine(S(0, 7));
+        }
+
+        public static BigInteger S(BigInteger m, BigInteger n)
+        {
+            //---------recursion
+
+            //if (m == 0)
+            //{
+            //    return 1;
+            //}
+            //else if (m == 1)
+            //{
+            //    return n;
+            //}
+
+            //BigInteger answer = 0;
+            //for (int k = 1; k <= n; k++)
+            //{
+            //    answer += S(m - 1, k);
+            //}
+
+            BigInteger[] arr = new BigInteger[(int)m + 1];
+            for (int i = 0; i <= m; i++)
+            {
+                arr[i] = 1;
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    arr[j] += arr[j - 1];
+                }
+            }
+
+            return arr[(int)m];
+        }
+    }
+}
---
+++ WhichAreIn/WhichAreIn/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhichAreIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(inArray(new string[] { "arp", "live", "strong" }, new string[] { "lively", "alive", "harp", "sharp", "armstrong" }));
+        }
+
+        public static string[] inArray(string[] array1, string[] array2)
+        {
+            List<string> r = new List<string>();
+
+            foreach (var s in array1)
+            {
+                foreach (var s2 in array2)
+                {
+                    if (s2.Contains(s))
+                    {
+                        r.Add(s);
+                    }
+                }
+            }
+
+            return r.Distinct().OrderBy(s => s).ToArray();
+        }
+    }
+}
