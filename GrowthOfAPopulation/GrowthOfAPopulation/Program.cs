e6a2a8dea6879e0e2d42403c05c23fc1894b00a9
nwenat <natala.dorozynska@gmail.com>
2021-08-02 08:20:32 +0200

    GrowthOfAPopulation

---
+++ GrowthOfAPopulation/GrowthOfAPopulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GrowthOfAPopulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NbYear(1500, 5, 100, 5000));
+        }
+
+        public static int NbYear(int p0, double percent, int aug, int p)
+        {
+            int years = 0;
+            int population = p0;
+            do
+            {
+                years++;
+                population = (int)(population * (1 + percent/100) + aug);
+            }
+            while (population < p);
+            return years;
+        }
+    }
+}
