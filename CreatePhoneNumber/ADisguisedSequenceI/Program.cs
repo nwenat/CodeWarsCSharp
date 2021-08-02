7cad83b787a9d149051af99c60d2085687832e6d
nwenat <natala.dorozynska@gmail.com>
2021-08-02 08:20:07 +0200

    CreatePhoneNumber

---
+++ CreatePhoneNumber/ADisguisedSequenceI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CreatePhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreatePhoneNumber(new int[]{ 1,2,3,4,5,6,7,8,9,0}));
+        }
+
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            return $"({numbers[0]}{numbers[1]}{numbers[2]}) {numbers[3]}{numbers[4]}{numbers[5]}-{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+        }
+    }
+}
