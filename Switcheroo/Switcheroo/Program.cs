cdade66de07d09c5e27db0942c380b5b72f96aad
nwenat <natala.dorozynska@gmail.com>
2021-08-04 08:38:20 +0200

    Switcherdo

---
+++ Switcheroo/Switcheroo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Switcheroo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Switcheroo("aaabcccbaaa"));
+        }
+
+        public static string Switcheroo(string x)
+        {
+            x = x.Replace('a', 'z').Replace('b', 'a').Replace('z', 'b');
+            return x;
+        }
+    }
+}
