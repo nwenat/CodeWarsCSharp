708ca248cfd6d2295ea3a703ba0d813c34be5cac
nwenat <natala.dorozynska@gmail.com>
2021-08-10 09:28:36 +0200

    SumOfCubes

---
+++ SumOfCubes/SumOfCubes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumOfCubes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumCubes(3));
+        }
+
+        public static long SumCubes(int n)
+        {
+            long sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += (long)Math.Pow(i, 3);
+            }
+            return sum;
+        }
+    }
+}
