6e2a5d1d43918c26e06f1ec4080eaf56a560c1f7
nwenat <natala.dorozynska@gmail.com>
2021-08-11 12:19:09 +0200

    BuyingACar

---
+++ BuyingACar/BuyingACar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BuyingACar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(nbMonths(2000, 8000, 1000, 1.5));
+        }
+
+        public static int[] nbMonths(int startPriceOld, int startPriceNew, int savingPerMonth, double percentLossByMonth)
+        {
+            int months = 0;
+            double balance = startPriceOld - startPriceNew;
+            double newCar = startPriceNew;
+            double oldCar = startPriceOld;
+
+            while (balance < 0)
+            {
+                percentLossByMonth += months % 2 == 1 ? 0.5 : 0;
+                months++;
+                newCar = newCar * (1 - (percentLossByMonth / 100));
+                oldCar = oldCar * (1 - (percentLossByMonth / 100));
+                balance = oldCar - newCar + savingPerMonth * months;
+            }
+            return new int[] {months, (int)Math.Round(balance)};
+        }
+    }
+}
