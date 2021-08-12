903ece92048e45db915c498a90d7a5f035f5f4c6
nwenat <natala.dorozynska@gmail.com>
2021-08-12 10:19:38 +0200

    RemoveDuplicateWords

---
+++ RemoveDuplicateWords/RemoveDuplicateWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace RemoveDuplicateWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveDuplicateWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"));
+        }
+
+        public static string RemoveDuplicateWords(string s)
+        {
+            return String.Join(" ", s.Split(' ', StringSplitOptions.None).Distinct());
+        }
+    }
+}
