d2f5d2af9001ba083dfe2d0806e0832a6acdd1a2
nwenat <natala.dorozynska@gmail.com>
2021-08-13 08:55:58 +0200

    BouncingBalls

---
+++ BouncingBalls/BouncingBalls/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BouncingBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(bouncingBall(30.0, 0.66, 1.5));
+        }
+
+        public static int bouncingBall(double h, double bounce, double window)
+        {
+            if (h > 0 && window < h && bounce > 0 && bounce < 1)
+            {
+                int result = 1;
+                int interation = 1;
+                while (h * Math.Pow(bounce, interation) > window)
+                { 
+                    interation++;
+                    result += 2;
+                }
+                return result;
+            }
+            return -1;
+        }
+    }
+}
