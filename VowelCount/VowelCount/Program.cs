85728af8dd5110a0fc96154ae7d6d7fd983375b7
nwenat <natala.dorozynska@gmail.com>
2021-08-16 08:19:18 +0200

    VowelCount

---
+++ VowelCount/VowelCount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VowelCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetVowelCount("abracadabra"));
+        }
+
+        public static int GetVowelCount(string str)
+        {
+            string s = str.Replace("a", "").Replace("e", "").Replace("i", "").Replace("o", "").Replace("u", "");
+            return str.Length - s.Length;
+        }
+    }
+}
