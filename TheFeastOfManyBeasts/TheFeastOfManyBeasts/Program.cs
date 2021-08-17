e97fddbe42267191bcab06a5e6a796a4c36f2b77
nwenat <natala.dorozynska@gmail.com>
2021-08-17 13:12:08 +0200

    TheFeastOfManyBeasts

---
+++ TheFeastOfManyBeasts/TheFeastOfManyBeasts/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TheFeastOfManyBeasts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Feast("great blue heron", "garlic naan"));
+        }
+
+        public static bool Feast(string beast, string dish)
+        {
+            return dish.StartsWith(beast[0]) && dish.EndsWith(beast[beast.Length-1]);
+        }
+    }
+}
