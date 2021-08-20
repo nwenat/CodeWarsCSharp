e285162d01de5dfe2676033879cde588d3c499d3
nwenat <natala.dorozynska@gmail.com>
2021-08-20 09:08:04 +0200

    SplitInParts

---
+++ SplitInParts/SplitInParts/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SplitInParts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SplitInParts("supercalifragilisticexpialidocious", 3));
+        }
+
+        public static string SplitInParts(string s, int partLength)
+        {
+            int j = 0;
+            string answer = s;
+            for (int i = 1; i <= s.Length / partLength; i++)
+            {
+                answer = answer.Insert(partLength * i + j, " ");
+                j++;
+            }
+            return answer.Trim();
+        }
+    }
+}
