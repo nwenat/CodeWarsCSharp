0a9b51159bccec47bbfe988ac2befa775f548b81
nwenat <natala.dorozynska@gmail.com>
2021-08-30 09:45:19 +0200

    PointInAUnitCircle

---
+++ PointInAUnitCircle/PointInAUnitCircle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PointInAUnitCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PointInCircle(1, 0));
+        }
+
+        public static bool PointInCircle(double x, double y)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) < 1; ;
+        }
+    }
+}
