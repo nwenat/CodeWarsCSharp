b39dce10eb3eb268d9d9b6dbfa569273022d8f64
nwenat <natala.dorozynska@gmail.com>
2021-08-31 18:44:43 +0200

    CharacterWithLongestConecutiveRepetition

---
+++ CharacterWithLongestConsecutiveRepetition/CharacterWithLongestConsecutiveRepetition/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CharacterWithLongestConsecutiveRepetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestRepetition("aaaabbaa"));
+        }
+
+        public static Tuple<char?, int> LongestRepetition(string input)
+        {
+            char? character = null;
+            int length = 0;
+
+            while(input.Length > 0)
+            {
+                char c = input[0];
+                int l = 0;
+                do
+                {
+                    input = input.Remove(0, 1);
+                    l++;
+                } while (input.StartsWith(c));
+
+                if(l > length)
+                {
+                    character = c;
+                    length = l;
+                }
+            }
+            return new Tuple<char?, int>(character, length);
+        }
+    }
+}
