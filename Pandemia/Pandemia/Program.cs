fde5cffb13190a5f11df9a5ef32d90a0be2731c4
nwenat <natala.dorozynska@gmail.com>
2021-09-01 09:33:04 +0200

    Pandemia

---
+++ Pandemia/Pandemia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pandemia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(infected("01000000X000X011X0X"));
+        }
+
+        public static double infected(string s)
+        {
+            int total = 0;
+            int infected = 0;
+            foreach (string continent in s.Split('X'))
+            {
+                total += continent.Length;
+                infected += continent.Contains('1') ? continent.Length : 0;
+            }
+            return total == 0 ? 0 : 100.0 * infected / total;
+        }
+    }
+}
