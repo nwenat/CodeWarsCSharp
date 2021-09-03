a746e7670842392d84e9e7b5f102d8f39d2bbd2f
nwenat <natala.dorozynska@gmail.com>
2021-09-03 08:37:19 +0200

    PyramidArray

---
+++ PyramidArray/PyramidArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PyramidArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Pyramid(2));
+        }
+
+        public static int[][] Pyramid(int n)
+        {
+            int[][] output = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                output[i] = new int[i+1];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    output[i][j] = 1;
+                }
+            }
+
+            return output;
+        }
+    }
+}
