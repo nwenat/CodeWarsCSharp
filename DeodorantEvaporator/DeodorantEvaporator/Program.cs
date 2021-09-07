e4e023a0a5c2120ee76b42b6654e14f8bf415858
nwenat <natala.dorozynska@gmail.com>
2021-09-07 10:36:15 +0200

    DeodorantEvaporator

---
+++ DeodorantEvaporator/DeodorantEvaporator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DeodorantEvaporator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(evaporator(10, 10, 10));
+        }
+
+        public static int evaporator(double content, double evap_per_day, double threshold)
+        {
+            int days = 0;
+            while (content * Math.Pow((100 - evap_per_day) / 100, days) > content * threshold / 100)
+            {
+                days++;
+            }
+            return days;
+        }
+    }
+}
