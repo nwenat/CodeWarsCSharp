c51323aa4948dacc8709edf5808abf0b81cbe3b0
nwenat <natala.dorozynska@gmail.com>
2021-09-08 08:44:43 +0200

    BuildTower

---
+++ BuildTower/BuildTower/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BuildTower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TowerBuilder(6);
+        }
+
+        public static string[] TowerBuilder(int nFloors)
+        {
+            string[] output = new string[nFloors];
+            int stars = 1;
+            int spaces = nFloors - 1;
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                output[i] = string.Concat(new string(' ', spaces), new string('*', stars), new string(' ', spaces));
+                stars += 2;
+                spaces--;
+            }
+            return output;
+        }
+    }
+}
