173cc3d5305d55c152269cf6e5322d8a06f5a925
nwenat <natala.dorozynska@gmail.com>
2021-09-10 09:38:40 +0200

    ArrayDiff

---
+++ ArrayDiff/ArrayDiff/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayDiff(new int[] { 1, 2, 3 }, new int[] { 1, 2 });
+        }
+
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            return a.Where(x => !b.Contains(x)).ToArray();
+        }
+    }
+}
