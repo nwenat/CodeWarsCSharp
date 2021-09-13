e5d8dec76b48c77afc1096655214e5450ffb7bd6
nwenat <natala.dorozynska@gmail.com>
2021-09-13 07:36:21 +0200

    EvenOrOddWhichIsGreater

---
+++ EvenOrOddWhichIsGreater/EvenOrOddWhichIsGreater/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace EvenOrOddWhichIsGreater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EvenOrOdd("123"));
+        }
+
+        public static string EvenOrOdd(string str)
+        {
+            int even = str.ToList().ConvertAll(x => int.Parse(x.ToString())).Where(x => x % 2 == 0).Sum();
+            int odd = str.ToList().ConvertAll(x => int.Parse(x.ToString())).Where(x => x % 2 != 0).Sum();
+            return even == odd ? "Even and Odd are the same" : odd > even ? "Odd is greater than Even" : "Even is greater than Odd";
+        }
+    }
+}
