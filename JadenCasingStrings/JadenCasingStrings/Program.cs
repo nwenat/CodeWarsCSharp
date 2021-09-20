7de7d82c6de4cc582c07a8f26f704845a3f3845c
nwenat <natala.dorozynska@gmail.com>
2021-09-20 08:49:41 +0200

    JadenCasingStrings

---
+++ JadenCasingStrings/JadenCasingStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace JadenCasingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToJadenCase("How can mirrors be real if our eyes aren't real"));
+        }
+
+        public static string ToJadenCase(string phrase)
+        {
+            return String.Join(' ', phrase.Split(' ', StringSplitOptions.None).ToList().ConvertAll(x => String.Concat(x[0].ToString().ToUpper(), x.Substring(1))));
+        }
+
+    }
+}
