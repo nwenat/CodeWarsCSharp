65996ac96d7cd0c088ad69ffb3ddcaa6bd1dc903
nwenat <natala.dorozynska@gmail.com>
2022-12-29 14:02:34 +0100

    Mean Means

---
+++ MeanMeans/MeanMeans/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeanMeans
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Geo_Mean(new int[] { 1, 2 }, 3));
+        }
+
+        public static double Geo_Mean(int[] nums, double arith_mean)
+        {
+            double number = arith_mean * (nums.Length + 1);
+            foreach (int num in nums)
+            {
+                number -= num;
+            }
+
+            double answer = number;
+
+            foreach (int num in nums)
+            {
+                answer *= num;
+            }
+
+            return (double)Math.Pow(answer, ((double)1 / (nums.Length + 1)));
+        }
+    }
+}
