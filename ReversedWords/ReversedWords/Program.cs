2141c7a19efec03fdd668b467917124deb6cd27d
nwenat <natala.dorozynska@gmail.com>
2023-01-11 13:32:01 +0100

    ReversedWords

---
+++ ReversedWords/ReversedWords/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("yoda doesn't speak like this"));
+
+        }
+
+        public static string ReverseWords(string str)
+        {
+            var list = str.Split(' ').ToList();
+
+            list.Reverse();
+            return String.Join(" ", list);
+
+            //string answer = "";
+            //foreach (var item in list)
+            //{
+            //    answer = item + " " + answer;
+            //}
+
+            //return answer.Trim();
+        }
+    }
+}
