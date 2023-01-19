53b34662fc1de830045efe793a7a0e7ad91251a5
nwenat <natala.dorozynska@gmail.com>
2023-01-19 10:13:08 +0100

    StringRepeat

---
+++ StringRepeat/StringRepeat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRepeat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RepeatStr(3, "tak"));
+
+        }
+
+        public static string RepeatStr(int n, string s)
+        {
+            string answer = "";
+            for (int i = 0; i < n; i++)
+            {
+                answer += s;
+            }
+            return answer;
+        }
+    }
+}
